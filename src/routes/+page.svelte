18cbb5786138e0bf435a90b0646affd5ec6b1d35
Erickype <erickype@hotmail.com>
2023-12-27 20:52:11 -0500

    Ui update to new fields

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
     import type { ActionData } from "./$types";
 
     import { VehicleTypes } from "$lib/constants";
+    import { VehicleModels } from "$lib/constants";
+    import { VehiclePrimaryUses } from "$lib/constants";
 
     export let form: ActionData;
 </script>
@@ -15,22 +17,41 @@
     <div class="grid">
         <div>
             <label for="vehicleType"> Vehicle type </label>
-            <select id="vehicleType" name="vehicleType" required>
+            <select id="vehicleType" name="vehicletype" required>
                 <option value="" selected>Select a vehicle type</option>
                 {#each VehicleTypes as vehicleType}
-                    <option value={vehicleType.value}>{vehicleType.name}</option>
+                    <option value={vehicleType.value}>{vehicleType.name}</option
+                    >
+                {/each}
+            </select>
+            <label for="vehicleModel"> Vehicle model </label>
+            <select id="vehicleModel" name="vehiclemodel" required>
+                <option value="" selected>Select a vehicle model</option>
+                {#each VehicleModels as vehicleModel}
+                    <option value={vehicleModel.value}
+                        >{vehicleModel.name}</option
+                    >
                 {/each}
             </select>
         </div>
         <div>
+            <label for="vehiclePrimaryUse"> Vehicle primary use </label>
+            <select id="vehiclePrimaryUse" name="vehicleprimaryuse" required>
+                <option value="" selected>Select a vehicle primary use</option>
+                {#each VehiclePrimaryUses as vehiclePrimaryUse}
+                    <option value={vehiclePrimaryUse.value}
+                        >{vehiclePrimaryUse.name}</option
+                    >
+                {/each}
+            </select>
             <label for="vehicleRegistrationNumber">
                 Vehicle registration number
                 <input
                     type="number"
                     id="vehicleRegistrationNumber"
-                    name="vehicleRegistrationNumber"
+                    name="vehicleregistrationnumber"
                     placeholder="Vehicle registration number"
-                    step="0.01"
+                    step="1"
                     min="1"
                     max="230"
                     required
